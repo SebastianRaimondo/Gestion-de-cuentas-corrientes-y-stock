8584ef3d44f4c363e45c769f5ae81b10e6598ed6
Sebastian Raimondo <sebyra@gmail.com>
2025-02-03 00:14:52 -0300

    Express funcionando

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const routes = require("./src/routes");
+const routes = require("../src/routes/index");
 
 const app = express();
 app.use(cors());
