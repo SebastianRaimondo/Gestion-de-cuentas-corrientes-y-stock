8a7adbb603e239d11148c91d8314af3bcc3407f1
Sebastian Raimondo <sebyra@gmail.com>
2025-02-02 22:52:37 -0300

    Inicialización del backend

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const routes = require("./src/routes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`🚀 Backend corriendo en http://localhost:${PORT}`);
+});
